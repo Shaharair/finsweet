f144205c02d7bfb0a349cdc155112e6ab883ed59
Shahariar <shahariarmd806@gmail.com>
2024-11-17 09:36:01 +0600

    demo

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -5,6 +5,7 @@ const Banner = () => {
     <section className="bg-ban">
         <div className="container px-3 lg:px-0 mx-auto bg-ban">
             <div className="">
+              
                 <p className="font-Roboto font-semibold text-[14px] text-[white] uppercase lg:pt-[100px] pt-[30px]">Welcome to our CHURCH</p>
                 <h3 className="font-Roboto font-bold lg:text-[54px] text-[24px] text-[white] uppercase lg:pr-[1000px] pt-[10px]">Become a part of our community</h3>
                 <div className="my-[30px]">
